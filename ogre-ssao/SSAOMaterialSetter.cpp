70395c0f5f76fcb2693144de5ef2ee513b973900
sevas <f.degroef@gmail.com>
2011-03-23 18:03:32 +0100

    overloads all loaded scene materials to add the geom buffer pass

--- ogre-ssao/SSAOMaterialSetter.cpp
+++ ogre-ssao/SSAOMaterialSetter.cpp
@@ -15,5 +15,19 @@ void SSAOMaterialSetter::CreatedEntity(const OgreMax::OgreMaxScene *scene, Ogre:
 {
     //entity->setMaterialName("SSAO/DiffuseLight_GBuffer");
 
-    //Ogre::MaterialPtr currentMaterial = entity->
+    int n = entity->getNumSubEntities();
+    for(int i=0 ; i<n ; i++)
+    {
+        Ogre::SubEntity *subEntity = entity->getSubEntity(i);
+        Ogre::MaterialPtr material = subEntity->getMaterial();
+
+        Ogre::Technique *geomTechnique = material->createTechnique();
+        geomTechnique->setSchemeName("geom");
+        geomTechnique->setName("geom");
+
+        Ogre::Pass *geomBufferPass = geomTechnique->createPass();
+        geomBufferPass->setVertexProgram("gbuffer_vs");
+        geomBufferPass->setFragmentProgram("gbuffer_ps");
+
+    }
 }
